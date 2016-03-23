636f9a2860c1ef081e0d22466c257beb223c3899
Baawa <Albin@dhcp-181115.eduroam.chalmers.se>
2016-03-23 10:06:13 +0100

    MainView + Renderer

---
+++ ChickenInvasion/app/src/main/java/app/android/chickeninvasion/MainViewRenderer.java
@@ -0,0 +1,27 @@
+package app.android.chickeninvasion;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by Albin on 2016-03-23.
+ */
+public class MainViewRenderer implements GLSurfaceView.Renderer {
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+    }
+}
