6637389d800fde4add7f56872429355811f0a263
kristofferek <inzeer@gmail.com>
2016-03-30 14:15:13 +0200

    ny shit

--- ChickenInvasion/app/src/main/java/app/android/chickeninvasion/controller/OpenGLActivity.java
+++ ChickenInvasion/app/src/main/java/app/android/chickeninvasion/controller/OpenGLActivity.java
@@ -3,6 +3,8 @@ package app.android.chickeninvasion.controller;
 import android.app.Activity;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import app.android.chickeninvasion.view.MainView;
 
@@ -14,12 +16,44 @@ public class OpenGLActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        // Turn off the window's title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // Super
         super.onCreate(savedInstanceState);
+
+        // Fullscreen mode
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         // Create a GLSurfaceView instance and set it
         // as the ContentView for this Activity.
         mGLView = new MainView(this);
         setContentView(mGLView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /*
+         * The activity must call the GL surface view's
+         * onResume() on activity onResume().
+         */
+        if (mGLView != null) {
+            mGLView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        /*
+         * The activity must call the GL surface view's
+         * onPause() on activity onPause().
+         */
+        if (mGLView != null) {
+            mGLView.onPause();
+        }
     }
 
 }
