2de95928287dc51a672fc71695c2c97733f60fd3
kristofferek <inzeer@gmail.com>
2016-04-13 21:31:52 +0200

    RAD

--- ChickenInvasion/app/src/main/java/app/android/chickeninvasion/model/TriangleTest.java
+++ ChickenInvasion/app/src/main/java/app/android/chickeninvasion/model/TriangleTest.java
@@ -1,6 +1,7 @@
 package app.android.chickeninvasion.model;
 
 import android.opengl.GLES20;
+import android.opengl.Matrix;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -13,44 +14,44 @@ import app.android.chickeninvasion.view.MainViewRenderer;
  */
 public class TriangleTest {
 
-    private FloatBuffer vertexBuffer;
-
-    private final String fragmentShaderCode =
-            "precision mediump float;" +
-                    "uniform vec4 vColor;" +
-                    "void main() {" +
-                    "  gl_FragColor = vColor;" +
-                    "}";
-
     private final String vertexShaderCode =
             // This matrix member variable provides a hook to manipulate
             // the coordinates of the objects that use this vertex shader
             "uniform mat4 uMVPMatrix;" +
-                    "attribute vec4 vPosition;" +
-                    "void main() {" +
-                    // the matrix must be included as a modifier of gl_Position
-                    // Note that the uMVPMatrix factor *must be first* in order
-                    // for the matrix multiplication product to be correct.
-                    "  gl_Position = uMVPMatrix * vPosition;" +
-                    "}";
 
-    // Use to access and set the view transformation
+                    "attribute vec4 vPosition;" + "void main() {" +
+                    // the matrix must be included as a modifier of gl_Position
+                    "  gl_Position = vPosition * uMVPMatrix;" + "}";
+
+    private final String fragmentShaderCode = "precision mediump float;"
+            + "uniform vec4 vColor;" + "void main() {"
+            + "  gl_FragColor = vColor;" + "}";
+
+    private final FloatBuffer vertexBuffer;
+    private final int mProgram;
+    private int mPositionHandle;
+    private int mColorHandle;
     private int mMVPMatrixHandle;
+
+    public float[] mModelMatrix = new float[16];
 
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
-    static float triangleCoords[] = {   // in counterclockwise order:
-            0.0f,  0.622008459f, 0.0f, // top
-            -0.5f, -0.311004243f, 0.0f, // bottom left
-            0.5f, -0.311004243f, 0.0f  // bottom right
-    };
-
-    private final int mProgram;
+    private final int vertexCount;
+    private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per
+    // vertex
 
     // Set color with red, green, blue and alpha (opacity) values
     float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 1.0f };
 
-    public TriangleTest() {
+    public TriangleTest(float xOffset) {
+        float triangleCoords[] = { // in counterclockwise order:
+                (0.0f+xOffset), 0.8f, 0.0f, // top
+                (-0.8f+xOffset), -0.8f, 0.0f, // bottom left
+                (0.8f+xOffset), -0.8f, 0.0f // bottom right
+        };
+
+        vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
         // initialize vertex byte buffer for shape coordinates
         ByteBuffer bb = ByteBuffer.allocateDirect(
                 // (number of coordinate values * 4 bytes per float)
@@ -65,32 +66,25 @@ public class TriangleTest {
         // set the buffer to read the first coordinate
         vertexBuffer.position(0);
 
+        // prepare shaders and OpenGL program
         int vertexShader = MainViewRenderer.loadShader(GLES20.GL_VERTEX_SHADER,
                 vertexShaderCode);
-        int fragmentShader = MainViewRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER,
-                fragmentShaderCode);
+        int fragmentShader = MainViewRenderer.loadShader(
+                GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
 
-        // create empty OpenGL ES Program
-        mProgram = GLES20.glCreateProgram();
+        Matrix.setIdentityM(mModelMatrix, 0);
 
-        // add the vertex shader to program
-        GLES20.glAttachShader(mProgram, vertexShader);
+        mProgram = GLES20.glCreateProgram(); // create empty OpenGL Program
+        GLES20.glAttachShader(mProgram, vertexShader); // add the vertex shader
+        // to program
+        GLES20.glAttachShader(mProgram, fragmentShader); // add the fragment
+        // shader to program
+        GLES20.glLinkProgram(mProgram); // create OpenGL program executables
 
-        // add the fragment shader to program
-        GLES20.glAttachShader(mProgram, fragmentShader);
-
-        // creates OpenGL ES program executables
-        GLES20.glLinkProgram(mProgram);
     }
 
-    private int mPositionHandle;
-    private int mColorHandle;
-
-    private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
-    private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
-
     public void draw(float[] mvpMatrix) {
-        // Add program to OpenGL ES environment
+        // Add program to OpenGL environment
         GLES20.glUseProgram(mProgram);
 
         // get handle to vertex shader's vPosition member
@@ -101,8 +95,7 @@ public class TriangleTest {
 
         // Prepare the triangle coordinate data
         GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX,
-                GLES20.GL_FLOAT, false,
-                vertexStride, vertexBuffer);
+                GLES20.GL_FLOAT, false, vertexStride, vertexBuffer);
 
         // get handle to fragment shader's vColor member
         mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
@@ -112,9 +105,11 @@ public class TriangleTest {
 
         // get handle to shape's transformation matrix
         mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+        MainViewRenderer.checkGlError("glGetUniformLocation");
 
-        // Pass the projection and view transformation to the shader
+        // Apply the projection and view transformation
         GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
+        MainViewRenderer.checkGlError("glUniformMatrix4fv");
 
         // Draw the triangle
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
--- ChickenInvasion/app/src/main/java/app/android/chickeninvasion/view/MainViewRenderer.java
+++ ChickenInvasion/app/src/main/java/app/android/chickeninvasion/view/MainViewRenderer.java
@@ -4,6 +4,10 @@ import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
 import android.os.SystemClock;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -15,9 +19,9 @@ import app.android.chickeninvasion.model.TriangleTest;
  */
 public class MainViewRenderer implements GLSurfaceView.Renderer {
 
-    private TriangleTest mTriangle;
+    private List<TriangleTest> currEnemies;
 
-    private float[] mRotationMatrix = new float[16];
+    private static final String TAG = "MyGLRenderer";
 
     // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
     private final float[] mMVPMatrix = new float[16];
@@ -27,7 +31,9 @@ public class MainViewRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onSurfaceCreated(GL10 neverUsed, EGLConfig config) {
         // initialize a triangle
-        mTriangle = new TriangleTest();
+        currEnemies = new ArrayList<>();
+        currEnemies.add(new TriangleTest(1.2f));
+        currEnemies.add(new TriangleTest(-1.8f));
     }
 
     @Override
@@ -38,31 +44,33 @@ public class MainViewRenderer implements GLSurfaceView.Renderer {
 
         // this projection matrix is applied to object coordinates
         // in the onDrawFrame() method
-        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 1, 15);
     }
 
     @Override
     public void onDrawFrame(GL10 neverUsed) {
-        float[] scratch = new float[16];
+            float[] scratch = new float[16];
+            // Draw background color
+            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
-        // Set the camera position (View matrix)
-        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+            // Set the camera position (View matrix)
+            Matrix.setLookAtM(mViewMatrix, 0, 0f, 0f, 15f, 0f, 0f, 0f, 0.0f, 1.0f, 0.0f);
 
-        // Calculate the projection and view transformation
-        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+        for (TriangleTest triangle : currEnemies){
+            Matrix.translateM(triangle.mModelMatrix, 0, 0f, 0f, 0.02f);
 
-        // Create a rotation transformation for the triangle
-        long time = SystemClock.uptimeMillis() % 4000L;
-        float angle = 0.090f * ((int) time);
-        Matrix.setRotateM(mRotationMatrix, 0, angle, 0, 0, -1.0f);
+            // Calculate the projection and view transformation
+            Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
 
-        // Combine the rotation matrix with the projection and camera view
-        // Note that the mMVPMatrix factor *must be first* in order
-        // for the matrix multiplication product to be correct.
-        Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
+            // Combine the model's translation & rotation matrix
+            // with the projection and camera view
+            // Note that the mMVPMatrix factor *must be first* in order
+            // for the matrix multiplication product to be correct.
+            Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, triangle.mModelMatrix, 0);
 
-        // Draw triangle
-        mTriangle.draw(scratch);
+            // Draw triangle
+            triangle.draw(scratch);
+        }
     }
 
     //Utility method
@@ -77,5 +85,27 @@ public class MainViewRenderer implements GLSurfaceView.Renderer {
         GLES20.glCompileShader(shader);
 
         return shader;
+    }
+
+    /**
+     * Utility method for debugging OpenGL calls. Provide the name of the call
+     * just after making it:
+     *
+     * <pre>
+     * mColorHandle = GLES20.glGetUniformLocation(mProgram, &quot;vColor&quot;);
+     * MyGLRenderer.checkGlError(&quot;glGetUniformLocation&quot;);
+     * </pre>
+     *
+     * If the operation is not successful, the check throws an error.
+     *
+     * @param glOperation
+     *            - Name of the OpenGL call to check.
+     */
+    public static void checkGlError(String glOperation) {
+        int error;
+        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+            Log.e(TAG, glOperation + ": glError " + error);
+            throw new RuntimeException(glOperation + ": glError " + error);
+        }
     }
 }
