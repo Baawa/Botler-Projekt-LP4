d4b51f4d08b9eb44683ef057bb7fbf6f49dc7d2a
Baawa <Albin@dhcp-177058.eduroam.chalmers.se>
2016-04-26 14:37:28 +0200

    flyg fågel flyg
    jaaaaaaaaa <3

--- ChickenInvasionlibGDX/core/src/com/chicken/invasion/ChickenInvasion.java
+++ ChickenInvasionlibGDX/core/src/com/chicken/invasion/ChickenInvasion.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
@@ -15,7 +16,6 @@ import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.utils.Array;
-import com.sun.tools.doclint.Entity;
 
 public class ChickenInvasion extends ApplicationAdapter implements InputProcessor{
 	SpriteBatch batch;
@@ -28,7 +28,7 @@ public class ChickenInvasion extends ApplicationAdapter implements InputProcesso
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 
-		this.world = new World(new Vector2(0, -10), true);
+		this.world = new World(new Vector2(0, 0), true);
 
 		pan = new ThrowableObject(0,0,300,300,"Pan",new Texture("pan100x100.png"),2.0,1, this.world);
 
@@ -39,24 +39,20 @@ public class ChickenInvasion extends ApplicationAdapter implements InputProcesso
 	public void render() {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
-		pan.sprite.draw(batch);
+		//pan.sprite.draw(batch);
 		//batch.draw(pan.getImage(),pan.x,pan.y);
 		Array<Body> bodies = new Array<Body>();
 		// Now fill the array with all bodies
 		world.getBodies(bodies);
 
 		for (Body b : bodies) {
-			// Get the body's user data - in this example, our user
-			// data is an instance of the Entity class
-			Entity e = (Entity) b.getUserData();
+			Sprite s = (Sprite) b.getUserData();
 
-			if (e != null) {
-				// Update the entities/sprites position and angle
-				e.setPosition(b.getPosition().x, b.getPosition().y);
-				// We need to convert our angle from radians to degrees
-				e.setRotation(MathUtils.radiansToDegrees * b.getAngle());
+			if (s != null){
+				s.setPosition(b.getPosition().x,b.getPosition().y);
+				s.draw(batch);
 			}
 		}
 		batch.end();
