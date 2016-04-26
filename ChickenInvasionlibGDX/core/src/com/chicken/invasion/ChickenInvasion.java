ff41cf5533ffa6e71f37ff33e97a31d2c49f9283
Baawa <Albin@dhcp-177058.eduroam.chalmers.se>
2016-04-26 14:23:51 +0200

    shit happens

--- ChickenInvasionlibGDX/core/src/com/chicken/invasion/ChickenInvasion.java
+++ ChickenInvasionlibGDX/core/src/com/chicken/invasion/ChickenInvasion.java
@@ -2,26 +2,107 @@ package com.chicken.invasion;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.utils.Array;
+import com.sun.tools.doclint.Entity;
 
-public class ChickenInvasion extends ApplicationAdapter {
+public class ChickenInvasion extends ApplicationAdapter implements InputProcessor{
 	SpriteBatch batch;
 	Texture img;
+	ThrowableObject pan;
+	World world;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		this.world = new World(new Vector2(0, -10), true);
+
+		pan = new ThrowableObject(0,0,300,300,"Pan",new Texture("pan100x100.png"),2.0,1, this.world);
+
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		pan.sprite.draw(batch);
+		//batch.draw(pan.getImage(),pan.x,pan.y);
+		Array<Body> bodies = new Array<Body>();
+		// Now fill the array with all bodies
+		world.getBodies(bodies);
+
+		for (Body b : bodies) {
+			// Get the body's user data - in this example, our user
+			// data is an instance of the Entity class
+			Entity e = (Entity) b.getUserData();
+
+			if (e != null) {
+				// Update the entities/sprites position and angle
+				e.setPosition(b.getPosition().x, b.getPosition().y);
+				// We need to convert our angle from radians to degrees
+				e.setRotation(MathUtils.radiansToDegrees * b.getAngle());
+			}
+		}
 		batch.end();
+
+		pan.currPoss();
+		world.step(1 / 60f, 6, 2);
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		pan.throwToPoint(100,200);
+		return true;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
